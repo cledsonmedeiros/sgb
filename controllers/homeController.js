b6ba508922f6eea8d6fbbf70b1ddc09c62a6f48d
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2019-06-18 22:00:28 -0300

    Reorganização das rotas e controlador home

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,70 +5,62 @@ module.exports = (app) => {
         return s.charAt(0).toUpperCase() + s.slice(1)
     };
 
-    var db = require('./../connect_db')();
+    let db = require('./../connect_db')();
 
     return {
-
-        login_page(req, res){
-          res.render('home/login', {titulo: "SGB - Login"});
-        },
-
-        cadastrar_page(req, res){
-            res.render('home/cadastrar', {
-                titulo: "SGB - Cadastrar",
-                pagina: "livro"
+        index_page(req, res) {
+            res.render('home/index', {
+                titulo: "SGB - Login",
             });
         },
-
-        criar_usuario_acao(req, res){
+        cadastrar_page(req, res) {
+            res.render('home/cadastrar', {
+                titulo: "SGB - Cadastro",
+            });
+        },
+        create_usuario(req, res) {
             req.body.usuario.nome = capitalize(req.body.usuario.nome.toString().toLowerCase());
             req.body.usuario.sobrenome = capitalize(req.body.usuario.sobrenome.toString().toLowerCase());
             req.body.usuario.login = req.body.usuario.login.toString().toLowerCase();
 
             db.model('usuario').insertMany(req.body.usuario, (error, result) => {
                 if (error) {
-                    res.render('home/cadastro', {titulo: "SGB - Cadastrar"});
+                    res.render('home/cadastrar', {titulo: "SGB - Cadastrar"});
                 }
-                res.render('home/login', {titulo: "SGB - Login"});
+                res.render('home/index', {titulo: "SGB - Login"});
             })
         },
-
-        login_acao(req, res) {
+        autenticar(req, res){
             var username = req.body.usuario.login.toString().trim();
             var password = req.body.usuario.senha.toString().trim();
 
             db.model('usuario').findOne({login: username, senha: password}, function (err, user) {
                 if (err){
                     console.log("Erro: " + err);
-                    res.render('home/login', {titulo: "SGB - Login"});
-                    return res.status(500).send();
+                    res.render('home/index', {titulo: "SGB - Login"});
                 }else {
                     if (!user){
                         console.log("Credenciais incorretas");
-                        res.render('home/login', {
+                        res.render('home/index', {
                             titulo: "SGB - Home"
                         });
-                        return res.status(404).send();
                     }else {
                         global.user = user;
-
-                        res.render('home/index', {
+                        res.render('home/home', {
                             titulo: "SGB - Home",
                             pagina: "home"
                         });
-                        return res.status(200).send();
                     }
                 }
             })
         },
-
-        logout_acao(req, res) {
+        logout(req, res) {
             req.session.destroy();
-            res.render('home/login', {titulo: "SGB - Login"});
+            res.render('home/index', {titulo: "SGB - Login"});
         },
 
         home_page(req, res) {
-            res.render('home/index', {
+            res.render('home/home', {
                 titulo: "SGB - Home",
                 pagina: "home"
             });
--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,15 @@
 module.exports = (app) => {
   const { homeController } = app.controllers;
 
-  // ROTAS LOGIN
-  app.get('/', homeController.login_page);
-
-  app.get('/login', homeController.login_page);
-  app.get('/index', homeController.login_page);
-  app.post('/entrar', homeController.login_acao);
-  app.get('/logout', homeController.logout_acao);
+  // NAVEGAÇÃO
+  app.get('/', homeController.index_page);
+  app.get('/index', homeController.index_page);
+  app.get('/cadastrar', homeController.cadastrar_page);
   app.get('/home', homeController.home_page);
 
-  // ROTAS CADASTRAR
-  app.get('/signup', homeController.cadastrar_page);
-  app.post('/criar_usuario', homeController.criar_usuario_acao);
+  // AÇÕES
+  app.post('/entrar', homeController.autenticar);
+  app.post('/create_usuario', homeController.create_usuario);
+  app.get('/logout', homeController.logout);
+
 };
